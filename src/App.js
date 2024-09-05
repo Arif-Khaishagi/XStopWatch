22390348d9a8d70a83a930454cc34df19629bb1c
Asif <asif.khan.khargone@gmail.com>
2024-09-05 09:36:58 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import StopWatch from './Stopwatch';
+// import Stopwatch from './watch';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Stopwatch/> */}
+      <StopWatch/>
+    </div>
+  );
+}
+
+export default App;
